cad888373cdfacf8edb739177640bd73c1c930ad
Josh Bavari <josh@raisemore.com>
2014-04-11 21:20:35 -0500

    Adding in node express web server, package.json for modules required

---
+++ app.js
@@ -0,0 +1,14 @@
+var fs = require("fs");
+var express = require("express");
+var logfmt = require("logfmt");
+var app = express();
+
+app.use(logfmt.requestLogger());
+app.use(express.static(__dirname + '/www'));
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
+console.log('Express server started - port: ' + port);
